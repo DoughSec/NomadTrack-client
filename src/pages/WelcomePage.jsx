1571cba701c53602dc0fcdd9f0a57790c5e9b6c1
DoughSec <addisondoseck@gmail.com>
2026-02-27 09:52:50 -0500

    added registration and rerouting configs to handle successful login/registration

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -3,10 +3,10 @@ import Header from '../component/Header.jsx';
 import GetStarted from '../component/GetStarted.jsx';
 import Hightlight from '../component/Highlight.jsx';
 
-export default function WelcomePage() {
+export default function WelcomePage({ isAuthenticated, setIsAuthenticated }) {
     return (
         <div className="welcome-page">
-            <Header />
+            <Header isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
             <GetStarted />
             <Hightlight />
             <Footer />
